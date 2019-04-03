9fd80a7f651520751d19f029ecb0bf8b62bc5239
Wojtek15420 <38695129+Wojtek15420@users.noreply.github.com>
2019-04-04 01:55:44 +0200

    Update SideThread.java

--- src/main/java/com/nels/SideThread.java
+++ src/main/java/com/nels/SideThread.java
@@ -1,21 +1,32 @@
 package com.nels;
 
 import javax.swing.*;
+import java.util.concurrent.RecursiveAction;
 
-public class SideThread extends Thread{
+public class SideThread extends RecursiveAction {
     private int width;
     private JFrame frame;
-
-    public SideThread(int w, JFrame f) {
+    private int number_of_forks;
+    public SideThread(int w, JFrame f, int nb_of_forks) {
         width = w;
         frame = f;
+        number_of_forks = nb_of_forks;
     }
 
     @Override
-    public void run() {
-        System.out.print("Thread id: "+ getId()+" is running.\n");
-        MyRegularComponent MRC = new MyRegularComponent((int)getId(),width);
-        frame.add(MRC);
+    protected void compute() {
+        if (number_of_forks<=1){
+            MyRegularComponent MRC = new MyRegularComponent(width);
+            frame.add(MRC);
+        }
+        else {
+            if(number_of_forks%2==0){
+                invokeAll(new SideThread(width, frame, number_of_forks/2),
+                        new SideThread(width, frame, number_of_forks/2));
+            }else{
+                invokeAll(new SideThread(width, frame, number_of_forks/2+1),
+                        new SideThread(width, frame, number_of_forks/2));
+            }
+        }
     }
-
 }
